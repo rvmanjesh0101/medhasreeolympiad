cb9f2536ddb58fd456e23ec14fcc40f7a0704390
rvmanjesh0101 <48276279+rvmanjesh0101@users.noreply.github.com>
2019-03-06 13:56:06 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function initMap() {
+//     var location = {lat: 13.072170, lng: 77.792220};
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center: location
+//     });
+// }
+
+// var marker = new google.maps.Marker({
+//     position: location,
+//     map: map
+// });
+
+
+
